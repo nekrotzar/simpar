91787372fe94c6f6d40b4cc0e8f30dfd3fc40df0
nekrotzar <luis.m.fonseca@hotmail.com>
2019-04-10 04:53:52 +0100

    Add system info to test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
+OS=`uname`
 PROGRAM=$1
 NUM_THREADS=${2:-4}
+EXE="$1"
+
+if [ ! -f "$EXE" ]; then
+    echo "$0: Executable '${EXE}' not found."
+	exit 1
+fi
+
+echo "===================================="
+echo 'System Info'
+echo "------------------------------------"
+
+if [[ "$OS" == "Darwin" ]]; then
+	echo 'OS: macOS'
+	echo 'Processor Vendor: ' `sysctl -n machdep.cpu.vendor`
+	echo 'Processor Name/Speed: ' `sysctl -n machdep.cpu.brand_string`
+	echo 'Total Number of Cores: ' `sysctl -n machdep.cpu.core_count`
+	echo 'Total Number of Threads: ' `sysctl -n machdep.cpu.thread_count`
+	echo 'Memory Size (MB):' "$((`sysctl -n hw.memsize` / (1024*1024)))"
+else
+	echo 'OS: Linux'
+	echo 'Processor Vendor: ' `cat /proc/cpuinfo | grep 'vendor' | uniq`
+	echo 'Processor Name/Speed: ' `cat /proc/cpuinfo | grep 'model name' | uniq`
+	echo 'Total Number of Cores: ' `cat /proc/cpuinfo | grep 'processor' | wc -l`
+	echo 'Memory Size (MB)' "$((`grep MemTotal /proc/meminfo | awk '{print $2}'` / 1024))"
+fi
+echo "===================================="
+echo
 
 if [[ -n "$PROGRAM" ]]; then
-	#Set number of threads
+	
+	# Create output directory if not exists
+	mkdir -p test/output
 	
 	if [[ "$PROGRAM" == "simpar" ]]; then
 		echo "MODE: serial"
@@ -19,7 +49,7 @@ if [[ -n "$PROGRAM" ]]; then
 
 	for filename in test/input/*.in
 	do
-		echo "__________________________________________"
+		echo "____________________________________"
 		COUNT=$((COUNT+1))
 				
 		# Get arguments from file
@@ -28,8 +58,7 @@ if [[ -n "$PROGRAM" ]]; then
 		done < "$filename"
 
 		echo "TEST #$COUNT : ./"$PROGRAM" "$name""
-		
-		time ./"$PROGRAM" $name > test/output/test$COUNT.out
+		echo "Execution times : " `{ time ./"$PROGRAM" $name > test/output/test$COUNT.out 2>&1 ; } 2>&1`
 
 		file1="test/validation/test$COUNT.txt"
 		file2="test/output/test$COUNT.out"
@@ -43,14 +72,13 @@ if [[ -n "$PROGRAM" ]]; then
 			echo "$STATUS"
 			FAILURE=$((FAILURE+1))
 		else
-			echo
 			echo "SUCCESS"
 			SUCCESS=$((SUCCESS+1))
 		fi
 	done
-	echo "__________________________________________"
+	echo "____________________________________"
 	echo "FINISHED: $SUCCESS out of $COUNT successfull tests"
-	echo "------------------------------------------"
+	echo "____________________________________"
 else
 	echo "Error passing arguments: must pass program name as argument, e.g ./run.test.sh simpar"
 fi
